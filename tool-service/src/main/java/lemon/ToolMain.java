a630e110313bf366fa8e351d0a61fe2e3286ee8e
陈育宏 <215272371@qq.com>
2020-09-11 17:14:11 +0800

    1.初始化项目

--- tool-service/src/main/java/lemon/ToolMain.java
+++ tool-service/src/main/java/lemon/ToolMain.java
@@ -3,6 +3,7 @@ package lemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author ：chenyuhong
@@ -12,6 +13,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class ToolMain {
     public static void main(String[] args) {
         SpringApplication.run(ToolMain.class, args);
